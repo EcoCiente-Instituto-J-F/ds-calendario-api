7160a6d4ea8eba65cca635f6363456272feeecbb
Emanuelly Mendes <manuellymendes79@gmail.com>
2026-08-03 07:16:53 -0300

    test: adiciona testes unitários e de controller para agendamentos

--- src/main/java/br/com/ecociente/calendario/core/exception/PerfilNaoAutorizadoException.java
+++ src/main/java/br/com/ecociente/calendario/core/exception/PerfilNaoAutorizadoException.java
@@ -1,10 +1,7 @@
 package br.com.ecociente.calendario.core.exception;
 
 public class PerfilNaoAutorizadoException extends RuntimeException {
-  private String perfil;
-
   public PerfilNaoAutorizadoException(String perfil) {
     super("Perfil não autorizado: " + perfil);
-    this.perfil = perfil;
   }
 }
